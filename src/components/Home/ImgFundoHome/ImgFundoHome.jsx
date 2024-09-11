a215a6cd2df89538e7f3c4ce8c4601481ff75ca6
Lucas Hideki Miyazaki <lucasmiyazaki6@gmail.com>
2024-09-11 14:01:21 -0300

    organizando caminhos

--- src/components/Home/ImgFundoHome/ImgFundoHome.jsx
+++ src/components/Home/ImgFundoHome/ImgFundoHome.jsx
@@ -1,7 +1,7 @@
 // Importa o arquivo de estilos CSS específico para a imagem da pagina home
 import './ImgFundoHome.css'
 // Importa a imagem do logo da aplicação
-import imgFundo from '../../assets/imgFundo.png'
+import imgFundo from '../../../assets/imgFundo.png'
 
 // Define a função Navegacao, que é um componente React responsável pela imagem da pagina home
 function ImgFundoHome() {
